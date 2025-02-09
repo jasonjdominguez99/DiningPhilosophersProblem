099c07b3249402cd684820a246c170eef1ae73b1
Jason Dominguez <jasonjdominguez99@gmail.com>
2025-02-09 00:02:32 +0000

    refactor: make `Fork` explicitly non-copyable & non-moveable

--- src/Fork.h
+++ src/Fork.h
@@ -8,6 +8,11 @@ public:
     explicit Fork(const unsigned int id) :
         m_id(id) {}
 
+    Fork(const Fork&) = delete;
+    Fork& operator=(const Fork&) = delete;
+    Fork(Fork&&) = delete;
+    Fork& operator=(Fork&&) = delete;
+
     // Satisfies the 'Lockable' concept
     void lock() { m_mutex.lock(); }
     void unlock() { m_mutex.unlock(); }
